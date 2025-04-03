6739ad52bd3708376846def17d2733428b70e081
rohit12551993 <rohitmadan1993@gmail.com>
2025-04-03 15:52:31 +0530

    Env variables

---
+++ cypress/e2e/testexecution.cy.js
@@ -0,0 +1,4 @@
+import "../Pages/testautomation"
+import "../Pages/google"
+
+
--- cypress/e2e/testteam.cy.js
+++ cypress/e2e/testteam.cy.js
@@ -3,10 +3,24 @@ import '@percy/cypress';
 describe("browser commands", () => {
     it.only("commands", () => {
 
-        cy.visit("https://www.google.com/");
-        cy.percySnapshot('google homepage');
+        cy.fixture("occupantdetails").then((data) => { // Loading fixture data
+            cy.visit("https://stg.risebuildings.com/pm/login"); // Open the website
+            cy.get("#rise-login-email").click().type(data.Email);// Enter email
+
+
+
+
+
+
+
+            cy.get("button[type='button']").click(); // Click the continue button
+            cy.get("#rise-login-password").click().type(data.Password); // Enter password
+            cy.get("button[type='button']").click(); // Click the sign-in button
+    
+    
       
-      
-    });
-  });
+        })
+    })
+
+  })
   
